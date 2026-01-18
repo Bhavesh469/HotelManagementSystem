9b785284c99069827992cc227908a901b49ab24f
Bhavesh Kumar <bhavesh469@gmail.com>
2026-01-18 18:40:33 +0500

    Add HowMany class

---
+++ HowMany.java
@@ -0,0 +1,11 @@
+public class HowMany {
+    private int number;
+
+    public HowMany(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
